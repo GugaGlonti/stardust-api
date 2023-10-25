ceffd2dca243180d7c4d3a3bb42cedc80d935137
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2023-10-25 20:16:18 +0200

    fix chats work with User Entities again

--- src/modules/chat/chat.controller.ts
+++ src/modules/chat/chat.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Query } from '@nestjs/common';
 
 import { ChatService } from './chat.service';
 import ErrorHandler from '../../common/classes/ErrorHandler';
+import { User } from '../users/user.entity';
 
 @Controller('chat')
 export class ChatController {
@@ -9,12 +10,19 @@ export class ChatController {
 
   @Get('/getChatIdentifier')
   async getChatIdentifier(
-    @Query() { friend, username }: { friend: string; username: string },
+    @Query()
+    {
+      friend: { username: friend },
+      username,
+    }: {
+      friend: User;
+      username: string;
+    },
   ) {
     try {
       const hash = await this.chatService.getChatId(friend, username);
       const lastMessage = await this.chatService.getLastMessage(hash);
-      return { id: hash, username, friend, lastMessage };
+      return { id: hash, username, friend: friend, lastMessage };
     } catch ({ message }) {
       ErrorHandler.handle(message);
     }
