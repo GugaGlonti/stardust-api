a15020da92d685f6c51f3d8ed135e497857e7e14
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2023-11-06 09:55:26 +0100

    minor

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,12 @@ export const jwtConstants = {
 };
 
 export const jokerConstants = {
-  //array of playing cards __, _0, _1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12, h13, h14, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14
+  //array of playing cards
+  // __, _0, _1,
+  // c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14,
+  // d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14,
+  // h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12, h13, h14,
+  // s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14
 
   //prettier-ignore
   cards: [
@@ -13,9 +18,10 @@ export const jokerConstants = {
     'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', 'h11', 'h12', 'h13', 'h14',
     's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
   ],
+
   //prettier-ignore
   jokerCards: [
-    '_0', '_1',
+    '__', '_0', '_1',
     'c6', 'c7', 'c8', 'c9', 'c10', 'c11', 'c12', 'c13', 'c14',
     'd6', 'd7', 'd8', 'd9', 'd10', 'd11', 'd12', 'd13', 'd14',
     'h6', 'h7', 'h8', 'h9', 'h10', 'h11', 'h12', 'h13', 'h14',
